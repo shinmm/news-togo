a559d4ed5f99f4bfe27715f8665f6fd018f5e08b
Shin Mitsuno <smitsuno@terpmail.umd.edu>
2019-12-06 00:00:23 -0500

    update routes for contact, about, home page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact Me')
