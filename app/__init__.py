2bbc1cbddc98df2a178cbefd55a6da9a0c05fc37
Shin Mitsuno <smitsuno@terpmail.umd.edu>
2019-12-06 12:55:17 -0500

    Implement user model with login form. update templates and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = b'/\xaa1\x16%\xe4\x81\x86\xb18RJ\xab\x92M\xee'
 
 #sql-alchemy config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+#Login
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 #create the database
 db.create_all()
