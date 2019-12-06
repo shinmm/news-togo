2ed7b2202b42ea284f238642efe428c5eb07eb42
Shin Mitsuno <smitsuno@terpmail.umd.edu>
2019-12-06 01:34:34 -0500

    implement user model, create db

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #sql-alchemy config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+#create the database
+db.create_all()
 
 from app import routes
 from app.models import *
--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
-from flask_app import db
+from app import db
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    news_preferences = db.Column(db.String(1000), nullable=False)
+
+    def __repr__(self):
+        return 'User Information: %s' % self.name
