2bfbb56edfc31d282f66da080f08441479e090c3
Shin Mitsuno <smitsuno@terpmail.umd.edu>
2019-12-06 00:28:16 -0500

    configure sql-alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#sql-alchemy config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 from app import routes
+from app.models import *
