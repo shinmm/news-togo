04079a80eb2d65828274e10f0c710141045cdd4f
Shin Mitsuno <smitsuno@terpmail.umd.edu>
2019-12-16 21:22:13 -0500

    add flask talisman

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_talisman import Talisman
 
 app = Flask(__name__)
+
+# Talisman configuration
+# CSP config
+csp = {
+    'default-src': [
+        "'self'",
+    ],
+    'script-src': [
+        'https://code.jquery.com/',
+        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/',
+        'https://stackpath.bootstrapcdn.com/bootstrap/'
+    ],
+    'style-src': [
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+        "'self'"
+    ],
+}
+
+talisman = Talisman(app, content_security_policy=csp)
+
+talisman.content_security_policy = csp
+talisman.content_security_policy_report_uri = "/csp_error_handling"
 
 app.config['SECRET_KEY'] = b'/\xaa1\x16%\xe4\x81\x86\xb18RJ\xab\x92M\xee'
 
